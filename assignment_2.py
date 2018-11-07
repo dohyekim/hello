8373dcadabb93e52566a9637874e0026efc4e3b9
dohyekim <dhleen5@hanmail.net>
2018-11-07 18:11:13 +0900

    first commit

--- assignment_2.py
+++ assignment_2.py
@@ -1,13 +1,10 @@
 # 1~100 중에서 소수(1과 자기자신)의 합을 구하시오.
 
-sum=0
-
-
-for i in range(3,101):
-    for n in range(2, i):
-        if(i % n == 0):
+for i in range(2,101):
+    for n in range(2,i):
+        if(i != 2 and n % i == 0):
             break
-        else:
-            ## i 가지고있기
-    sum += i
+        if(n == (i-1) and i == 2):
+            sum += i
+
 print(sum)

8f931fc729dedf86fb925bc9dd0dde3a3f66d851
dohyekim <dhleen5@hanmail.net>
2018-11-12 22:06:42 +0900

    first commit

---
+++ lesson_ibuiltn_function/11_global_variables.py
@@ -0,0 +1,12 @@
+# Block 내에서 global keyword가 없을시 Local variable!!
+# (별도의 메모리 공간에 위치)
+
+g_x = 1000
+
+def change_x():
+    #global g_x
+    g_x = 300
+    print("inner def>>", g_x)
+
+change_x()
+print("222>>", g_x)

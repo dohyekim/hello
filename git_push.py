65f4ae11688fd54f54133d30737b8d164314abac
dohyekim <dhleen5@hanmail.net>
2018-11-14 16:12:31 +0900

    2018-11-14 - 강의

---
+++ git_push.py
@@ -0,0 +1,24 @@
+import sys
+import os
+import datetime
+
+sa = sys.argv #[0] 실행파일,  [1] 메시지 
+
+now = datetime.datetime.now()
+default_msg = "{} - 강의".format(now.strftime('%Y-%m-%d'))
+commit_msg = default_msg
+has_msg = len(sa) >= 2
+
+if has_msg == False:
+    input_msg =input("Default Message?? (Yes:Enter or input message)")
+    if input_msg != '':
+        commit_msg = input_msg
+
+def git_push():
+
+    os.system("git add --all")
+    os.system("git commit -am \"{}\"".format(commit_msg))
+    os.system("git push")
+
+git_push()
+
--- lesson_module/try_this_10.py
+++ lesson_module/try_this_10.py
@@ -1,10 +1,11 @@
+
 import sys
 import os
 
 msg= input("메시지를 입력하세요 >>>")
 
 
-def git_push():
+#def git_push():
     os.system("git add --all")
     os.system("git commit -am \"{}\"".format(msg))
     os.system("git push")

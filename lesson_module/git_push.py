ffc5e085ac8cfefe455a00a75badc62c3f11c819
dohyekim <dhleen5@hanmail.net>
2018-11-14 15:48:56 +0900

    first commit

--- lesson_module/git_push.py
+++ lesson_module/git_push.py
@@ -7,6 +7,6 @@ msg= input("메시지를 입력하세요 >>>")
 def git_push():
     os.system("git add --all")
     os.system("git commit -am \"{}\"".format(msg))
-    os.system(")git push")
+    os.system("git push")
 
 git_push()

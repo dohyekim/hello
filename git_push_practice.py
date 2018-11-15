7534190b46fcc22719989ba590343ee3f4c1ae27
dohyekim <dhleen5@hanmail.net>
2018-11-15 11:20:17 +0900

    2018-11-15 강의

--- git_push_practice.py
+++ git_push_practice.py
@@ -5,7 +5,7 @@ import datetime
 
 def git_push():
     os.system("git add --all")
-    os.system("git commit -am {}".format(input_msg))
+    os.system("git commit -am \"{}\"".format(input_msg))
     os.system("git push")
 
 sa = sys.argv
@@ -14,11 +14,13 @@ now = datetime.datetime.now()
 default_msg = "{} 강의".format(now.strftime('%Y-%m-%d'))
 
 if len(sa) == 2:
-    input_msg = sys.argv[1]
+    input_msg = sa[1]
 
 if len(sa) <2:
-    add_msg = input("Default? if yes : enter, no : message")
+    add_msg = input("Default? if yes : enter, no : message ")
     if add_msg == '':
         input_msg = default_msg
     else:
         add_msg = input_msg
+
+git_push()

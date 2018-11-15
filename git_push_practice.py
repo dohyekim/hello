f5dc0ee813400f649bda166c480de6e7cc6e8b68
dohyekim <dhleen5@hanmail.net>
2018-11-15 14:00:12 +0900

    2018-11-15 강의

--- git_push_practice.py
+++ git_push_practice.py
@@ -3,9 +3,9 @@ import os
 import datetime
 
 
-def git_push():
+def git_push(msg):
     os.system("git add --all")
-    os.system("git commit -am \"{}\"".format(input_msg))
+    os.system("git commit -am \"{}\"".format(msg))
     os.system("git push")
 
 sa = sys.argv
@@ -13,6 +13,7 @@ now = datetime.datetime.now()
 
 
 default_msg = "{} 강의".format(now.strftime('%Y-%m-%d')) # default_msg가 여기서 먼저 선언된 후에 input_msg = default_msg가 되어야 한다.
+input_msg = default_msg                 # input_msg = default_msg가 없으면 input_msg가 defined되지 않았다고 뜬다.
  
 
 if len(sa) == 2:
@@ -23,6 +24,8 @@ if len(sa) <2:
     if add_msg == '':
         input_msg = default_msg
     else:
-        add_msg = input_msg
+        input_msg = add_msg             # add_msg를 input_msg에 담는다. #add_msg = input_msg  는 잘못된 코드
 
-git_push()
+git_push(input_msg)
+
+# 주석은 간단하지 않은 경우에는 상단에 적는다.

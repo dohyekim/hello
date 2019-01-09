0c24ebf01333719ab86301c55464f095b05bd933
dohyekim <dhleen5@hanmail.net>
2019-01-09 10:05:44 +0900

    1.9

--- data/urllib_retrieve.py
+++
@@ -1,9 +0,0 @@
-# download -  image
-
-import urllib.request as ur
-
-url = "http://www.weather.go.kr/repositary/image/sat/coms/coms_mi_le1b_ir1_k_201901080330.thn.png"
-
-saveFile = "./images/weather1.png"
-ur.urlretrieve(url, saveFile)
-print("Ok!")

df2b612990ea69b5690b89e1cb536ea3770d942b
dohyekim <dhleen5@hanmail.net>
2019-04-19 09:51:31 +0900

    ml

--- ml/iris_best.py
+++ ml/iris_best.py
@@ -3,13 +3,13 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import GridSearchCV
 
-csv = pd.read_csv('data/iris.csv')
+# 최적의 값 찾기 ############
 
+csv = pd.read_csv('data/iris.csv')
 
 irisData = csv[['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth']]
 irisLabel = csv['Name']
 
-# grid-search params
 params = [
     {"C": [1, 10, 100, 1000], "kernel": ['linear']},
     {"C": [1, 10, 100, 1000], "kernel": ['rbf'], "gamma": [0.001, 0.0001]},
@@ -18,38 +18,20 @@ params = [
 trainData, testData, trainLabel, testLabel = train_test_split(irisData, irisLabel)
 clf = GridSearchCV(svm.SVC(), params, n_jobs = -1, cv = 3, iid=True)
 clf.fit(trainData, trainLabel)
-print("machine=", clf.best_estimator_)
+print(clf.best_estimator_)
 
-pred = clf.predict(testData)
-score = metrics.accuracy_score(testLabel, pred)
+# 최적의 값 적용하기 ##########
 
-# print(score)
-
-
-# machine= SVC(C=1, cache_size=200, class_weight=None, coef0=0.0,
-#   decision_function_shape='ovr', degree=3, gamma='auto_deprecated',
-#   kernel='linear', max_iter=-1, probability=False, random_state=None,
-#   shrinking=True, tol=0.001, verbose=False)
-# clff = svm.SVC(C=1000, cache_size=200, class_weight=None, coef0=0.0,
-#   decision_function_shape='ovr', degree=3, gamma=0.001, kernel='rbf',
-#   max_iter=-1, probability=False, random_state=None, shrinking=True,
-#   tol=0.001, verbose=False)
-clff = svm.SVC(C=1, cache_size=200, class_weight=None, coef0=0.0,
+clfBest = svm.SVC(C=1, cache_size=200, class_weight=None, coef0=0.0,
   decision_function_shape='ovr', degree=3, gamma='auto_deprecated',
   kernel='linear', max_iter=-1, probability=False, random_state=None,
   shrinking=True, tol=0.001, verbose=False)
-clff.fit(trainData, trainLabel)
-# print("machine=", clff.best_estimator_)
 
-pred = clff.predict(testData)
+
+clfBest.fit(trainData, trainLabel)
+
+pred = clfBest.predict(testData)
 score = metrics.accuracy_score(testLabel, pred)
 
 print(score)
 
-
-# while True:
-#     cmd = input("꽃받침의 길이, 꽃받침의 넓이, 꽃잎의 길이, 꽃잎의 넓이 >> ")
-#     if not cmd: break
-#     a, b, c, d = cmd.split(',')
-#     p = clf.predict([[a, b, c, d]])
-#     print("이름 = ", p[0])
--- ml/mnistexam.py
+++ ml/mnistexam.py
@@ -1,4 +1,4 @@
-from sklearn import svm, metrics    
+from sklearn import svm, metrics
 import pandas as pd
 from sklearn.externals import joblib
 from pathlib import Path
@@ -6,23 +6,30 @@ from pathlib import Path
 def readCsv(file, maxcnt):
     labels = []
     images = []
-    with open(file, "r") as f:
+    with open(file, "r", encoding="utf-8") as f:
         for i, line in enumerate(f):
             if i >= maxcnt:
                 break
             cols = line.split(",")
-            labels.append(int(cols.pop(0)))
-            images.append(list(map(lambda b: int(b) / 256, cols))) 
+            labels.append(int(cols.pop(0)))      # 첫번째 자리가 label
+            images.append(list(map(lambda b: int(b) / 256, cols)))  # [[]]
     return {"labels": labels, "images": images}
 
 
-train = readCsv('./data/train.csv', 60000)  
 test = readCsv('./data/t10k.csv', 10000)
 
+pklFile = "./data/mnist.pkl"
+clf = None
+if Path(pklFile).exists():
+    print("File Exists!!")
+    clf = joblib.load(pklFile)
 
 # training ---------------------------
-clf = svm.SVC(gamma='auto')
-clf.fit(train['images'], train['labels'])
+if not clf:
+    train = readCsv('./data/train.csv', 60000)
+    clf = svm.SVC(gamma='auto')
+    clf.fit(train['images'], train['labels'])
+    joblib.dump(clf, pklFile)
 
 # test -------------------------
 pred = clf.predict(test['images'])

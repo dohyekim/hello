996b8ab4168a4bb90750cbaff29a53f04df94371
dohyekim <dhleen5@hanmail.net>
2019-09-06 15:01:33 +0900

    doubly_linked_list

---
+++ data_structure/doubly_linked_list.py
@@ -0,0 +1,82 @@
+class Node:
+
+    def __init__(self, item):
+        self.data = item
+        self.prev = None
+        self.next = None
+
+
+class DoublyLinkedList:
+
+    def __init__(self):
+        self.nodeCount = 0
+        self.head = Node(None)
+        self.tail = Node(None)
+        self.head.prev = None
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        self.tail.next = None
+
+
+    def reverse(self):
+        result = []
+        curr = self.tail.prev
+        i = 1
+        while (i <= self.nodeCount):
+            result.append(curr.data)
+            curr = curr.prev
+            i += 1
+        return result
+
+    def getAt(self, pos):
+        if pos < 0 or pos > self.nodeCount:
+            return None
+
+        if pos > self.nodeCount // 2:
+            i = 0
+            curr = self.tail
+            while i < self.nodeCount - pos + 1:
+                curr = curr.prev
+                i += 1
+        else:
+            i = 0
+            curr = self.head
+            while i < pos:
+                curr = curr.next
+                i += 1
+
+        return curr
+
+
+    def insertAfter(self, prev, newNode):
+        next = prev.next
+        newNode.prev = prev
+        newNode.next = next
+        prev.next = newNode
+        next.prev = newNode
+        self.nodeCount += 1
+        return True
+
+
+    def insertAt(self, pos, newNode):
+        if pos < 1 or pos > self.nodeCount + 1:
+            return False
+
+        prev = self.getAt(pos - 1)
+        return self.insertAfter(prev, newNode)
+
+
+def solution(x):
+    return 0
+
+
+# interactive shell에서 연습용
+a = Node(24)
+b = Node(36)
+c = Node(72)
+d = Node(84)
+L = DoublyLinkedList()
+L.insertAt(1,a)
+L.insertAt(2,b)
+L.insertAt(3,c)
+L.insertAt(4, d)

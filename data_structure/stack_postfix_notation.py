9144e09286c8078c3d1c0963e0c40876e53aec58
dohyekim <dhleen5@hanmail.net>
2019-09-16 23:44:17 +0900

    stack

---
+++ data_structure/stack_postfix_notation.py
@@ -0,0 +1,55 @@
+class ArrayStack:
+
+    def __init__(self):
+        self.data = []
+
+    def size(self):
+        return len(self.data)
+
+    def isEmpty(self):
+        return self.size() == 0
+
+    def push(self, item):
+        self.data.append(item)
+
+    def pop(self):
+        return self.data.pop()
+
+    def peek(self):
+        return self.data[-1]
+
+prec = {
+    '*': 3, '/': 3,
+    '+': 2, '-': 2,
+    '(': 1
+}
+
+def solution(S):
+    opStack = ArrayStack()
+    answer = ''
+    for x in S:
+        if x == '(':
+            opStack.push(x)
+            continue
+        if x in prec: # 연산자인 경우
+            if opStack.isEmpty():
+                opStack.push(x)
+            else:
+                if (prec[opStack.peek()] >= prec[x]):
+                    last = str(opStack.pop())
+                    answer += last
+                else:
+                    opStack.push(x)
+        elif x == ')': # )인 경우
+            while not opStack.isEmpty():
+                last = opStack.pop()
+                if last != '(':
+                    answer += last
+        else: # 알파벳인 경우
+            answer += str(x)
+        
+        while not opStack.isEmpty():
+            answer += str(opStack.pop())
+            
+        
+    return answer
